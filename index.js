d9fb3578496f2b97d91160b1ffb64cd88d6ff730
Oleg Mihailik <mihailik@gmail.com>
2024-01-14 23:24:59 +0000

    Hydrating shards.

---
+++ index.js
@@ -0,0 +1,112 @@
+async function coldskyDIDs() {
+  const statusBar = document.querySelector('.status-content');
+  const payload = document.querySelector('.payload');
+
+  await load();
+
+  async function load() {
+    statusBar.textContent = 'Detecting cursors...';
+
+    /** @type {import('./cursors.json')} */
+    const cursors = await fetch('https://dids.colds.ky/cursors.json').then(x => x.json());
+
+    statusBar.textContent = 'Hydrating...';
+
+    payload.className = 'payload payload-show';
+    const didsPanel = document.querySelector('.dids-panel');
+    const shardsPanel = document.querySelector('.shards-panel');
+    const gitAuthPanel = document.querySelector('.git-auth-panel');
+
+    loadDIDs(cursors, didsPanel);
+    loadShards(cursors, shardsPanel);
+  }
+
+  async function loadDIDs(cursors, didsPanel) {
+  }
+
+  async function loadShards(cursors, shardsPanel) {
+    const letters = '234567abcdefghjiklmnopqrstuvwxyz';
+
+    const miniTitle = document.createElement('h3');
+    miniTitle.className = 'mini-title';
+    miniTitle.textContent = 'Shards';
+    shardsPanel.appendChild(miniTitle);
+
+    const matrix = document.createElement('div');
+    matrix.className = 'matrix';
+
+    const matrixElements = {};
+    const matrixPromises = {};
+    let successCount = 0;
+    let errorCount = 0;
+    for (let iFirstLetter = 0; iFirstLetter < letters.length; iFirstLetter++) {
+      const firstLetter = letters[iFirstLetter];
+      for (let iSecondLetter = 0; iSecondLetter < letters.length; iSecondLetter++) {
+        const secondLetter = letters[iSecondLetter];
+        const key = firstLetter + secondLetter;
+        const element = document.createElement('div');
+        element.className = 'matrix-element rnd-' + (Math.random() > 0.5 ? '1' : '2');;
+        element.style.gridColumn = String(iFirstLetter + 1);
+        element.style.gridRow = String(iSecondLetter + 1);
+        matrixElements[key] = element;
+        matrix.appendChild(element);
+
+        matrixPromises[key] = loadShard(element, key);
+      }
+    }
+
+    shardsPanel.appendChild(matrix);
+
+    await Promise.all(Object.values(matrixPromises));
+
+    const matrixShards = {};
+    for (const key of Object.keys(matrixPromises)) {
+      matrixShards[key] = await matrixPromises[key];
+    }
+
+    function updateTitle() {
+      miniTitle.textContent =
+        successCount ?
+        (
+          successCount.toLocaleString() + ' shards' +
+          (errorCount ? ', ' + errorCount.toLocaleString() + ' retry' : '')
+        ) :
+        errorCount ? 'Shards: ' + errorCount.toLocaleString() + ' retry' : 'Shards';
+    }
+
+    async function loadShard(matrixElement, shardKey) {
+      const baseClass = matrixElement.className;
+
+      const start = Date.now();
+      let errorReported = false;
+
+      while (true) {
+        await new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 600));
+        matrixElement.className = baseClass + ' loading';
+        try {
+          const shardData = await fetch('https://dids.colds.ky/' + shardKey[0] + '/' + shardKey + '.json').then(x => x.json());
+          matrixElement.className = baseClass + ' loaded';
+          if (errorReported) errorCount--;
+          successCount++;
+          updateTitle();
+          return shardData;
+        } catch (error) {
+          if (!errorReported) {
+            errorCount++;
+            errorReported = true;
+            updateTitle();
+          }
+
+          matrixElement.className = baseClass + ' error';
+          const waitFor = Math.max(
+            30000,
+            Math.min(300, (Date.now() - start) / 3)
+          ) * (0.7 + Math.random() * 0.6);
+
+          await new Promise(resolve => setTimeout(resolve, waitFor));
+        }
+      }
+    }
+  }
+
+} coldskyDIDs();
