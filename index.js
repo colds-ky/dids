bf38144daca6a0784bc10e6c26835f0aa6263ee0
Oleg Mihailik <mihailik@gmail.com>
2024-01-20 17:55:12 +0000

    With webcommit.

--- index.js
+++ index.js
@@ -40,25 +40,19 @@ async function coldskyDIDs() {
             throw new Error('Please provide a GitHub personal access token');
           }
 
-          const octokit =
-            // @ts-ignore
-            new Octokit({ auth: authToken });
-          const user = await octokit.rest.users.getAuthenticated();
-          console.log('auth user: ', user);
-
-          const commitResponse = await octokit.rest.repos.getCommit({
+          const prepare = await webcommit({
+            auth: authToken,
             owner: 'colds-ky',
             repo: 'dids',
-            ref: 'main'
+            branch: 'main'
           });
-          console.log('commit: ', commitResponse);
+          console.log('commit: ', prepare);
 
-          githubCommitStatus.textContent = 'Preparing files...';
+          githubCommitStatus.textContent = 'Preparing summary...';
 
-          const changeFiles = {};
-
-          // seed cursors with latest
-          changeFiles['cursors.json'] = JSON.stringify({
+          await prepare.put(
+            'cursors.json',
+            JSON.stringify({
             ...cursors,
             listRepos: {
               cursor: reflectCursor,
@@ -71,25 +65,36 @@ async function coldskyDIDs() {
                   ' ' +
                   (/mobile/i.test(navigator.userAgent) ? 'mobile ' : '') +
                   navigator.userAgent.trim().split(/\s+/g).slice(-1)[0]) +
-                (commitResponse?.data?.sha ? ' git:?' :
-                  ' git:' + commitResponse?.data?.sha?.slice(0, 7))
+                (!prepare.head.sha ? ' git:?' :
+                  ' git:' + prepare.head.sha.slice(0, 7))
             }
-          }, null, 2);
+          }, null, 2));
+
+          githubCommitStatus.textContent = 'Updating files...';
 
           let totalFiles = 0;
           let incrementChars = 0;
           let totalChars = 0;
-          for (const twoLetterKey in bucketsAndElements) {
-            const entry = bucketsAndElements[twoLetterKey];
-            if (!entry.bucket.originalJSONText || !entry.bucket.newShortDIDs?.size) {
+          const updatedFiles = [];
+          const singleSet = new Set();
+          for (const twoLetterKey in renderedBuckets.buckets) {
+            const bucket = renderedBuckets.buckets[twoLetterKey];
+            if (!bucket.originalJSONText || !bucket.newShortDIDs?.size) {
               console.log('JSON text is not retrieved: ' + twoLetterKey);
               continue;
             }
 
-            const lead = entry.bucket.originalJSONText.replace(/\s*\]\s*$/, '');
-            const incrementJSON = packDidsJson([...entry.bucket.newShortDIDs], ',\n', '\n]\n');
+            const lead = bucket.originalJSONText.replace(/\s*\]\s*$/, '');
+            const incrementJSON = packDidsJson([...bucket.newShortDIDs], ',\n', '\n]\n');
             const path = getShardBucketPath(twoLetterKey);
-            changeFiles[path] = lead + incrementJSON;
+
+            await prepare.put(path, lead + incrementJSON);
+
+            updatedFiles.push(path);
+
+            singleSet.clear();
+            singleSet.add(twoLetterKey);
+            renderedBuckets(singleSet, true);
 
             incrementChars += incrementJSON.length;
             totalFiles++;
@@ -101,30 +106,20 @@ async function coldskyDIDs() {
             ' +' + incrementChars.toLocaleString() + 'ch ' +
             totalChars.toLocaleString() + ' total...';
 
-          console.log('changeFiles: ', changeFiles);
-
-          const { totalKnownDids, newDids } = getTotals();
+          console.log('changeFiles: ', updatedFiles);
 
           const commitMessage =
-            '+' + newDids.toLocaleString() + ' to ' + totalKnownDids.toLocaleString() + ' dids';
+            pumpingState.knownAccounts.toLocaleString() +
+            '+' + pumpingState.newAccounts.toLocaleString() +
+            ' to ' + (pumpingState.knownAccounts + pumpingState.newAccounts).toLocaleString() + ' dids';
 
-          await octokit.createOrUpdateFiles({
-            owner: 'colds-ky',
-            repo: 'dids',
-            branch: 'main',
-            changes: [
-              {
-                message: commitMessage,
-                files: changeFiles
-              }
-            ],
-          });
+          await prepare.commit(commitMessage);
 
           const completeLabel = document.createElement('div');
           completeLabel.className = 'complete-label';
           completeLabel.textContent = 'Complete.';
           githubCommitStatus.appendChild(completeLabel);
-          githubCommitStatus.textContent = 'Committed new accounts.';
+          githubCommitStatus.textContent = 'Committed ' + commitMessage + '.';
 
         } catch (error) {
           gitAuthPanel.classList.remove('github-commit-in-progress');
@@ -195,17 +190,24 @@ async function coldskyDIDs() {
         }
       }
 
+      updateBucket.buckets = {};
       for (const renderer of Object.values(rendererByTwoLetterKey)) {
         matrixElement.appendChild(renderer.element);
+        updateBucket.buckets[renderer.bucket.twoLetterKey] = renderer.bucket;
       }
 
-      return updateBuckets;
+      return /** @type {typeof updateBuckets & { buckets: { [twoLetterKey: string]: BucketData }}} */(
+        updateBuckets
+      );
 
-      /** @param {{ has(twoLetterKey: string): boolean  }} [recentBuckets] */
-      function updateBuckets(recentBuckets) {
+      /**
+       * @param {{ has(twoLetterKey: string): boolean  }} [recentBuckets]
+       * @param {boolean} [commit]
+       */
+      function updateBuckets(recentBuckets, commit) {
         for (const renderer of Object.values(rendererByTwoLetterKey)) {
           const recent = !!recentBuckets?.has(renderer.bucket.twoLetterKey);
-          renderer.update(recent);
+          renderer.update(recent, recent && !!commit);
         }
       }
 
@@ -222,6 +224,7 @@ async function coldskyDIDs() {
         this.loadedClass = false;
         this.hasUpdatesClass = false;
         this.hasRecentUpdatesClass = false;
+        this.committing = false;
         this.update(false);
         rendererByTwoLetterKey[twoLetterKey] = this;
       }
@@ -229,8 +232,9 @@ async function coldskyDIDs() {
       /**
        * @this {BucketRenderer}
        * @param {boolean} recentUpdates
+       * @param {boolean} [asCommit]
        */
-      function updateBucket(recentUpdates) {
+      function updateBucket(recentUpdates, asCommit) {
         const errorClass = !!this.bucket.error;
         const loadedClass = !isPromise(this.bucket.originalShortDIDs);
         const hasUpdatesClass = !!this.bucket.newShortDIDs?.size;
@@ -253,6 +257,11 @@ async function coldskyDIDs() {
         if (recentUpdates !== this.hasRecentUpdatesClass) {
           this.element.classList.toggle('fresh', recentUpdates);
           this.hasRecentUpdatesClass = recentUpdates;
+        }
+
+        if (asCommit !== this.committing) {
+          this.element.classList.toggle('committing', !!asCommit);
+          this.committing = !!asCommit;
         }
       }
 
@@ -683,6 +692,179 @@ async function coldskyDIDs() {
   /** @param {string} url */
   function relativeURL(url) {
     return /http/i.test(location.protocol || '') ? url : 'https://dids.colds.ky/' + url;
+  }
+
+  // @ts-check
+
+  /**
+   * @typedef {{
+   *  owner: string,
+   *  repo: string,
+   *  branch?: string,
+   *  fetch?: typeof fetch,
+   *  auth?: string,
+   *  octokit?: import("@octokit/rest").Octokit
+   * }} PrepareParams
+   */
+
+  /**
+   * @typedef {{
+   *  head: CommitData,
+   *  put(file: string, content: string | ArrayBuffer | Uint8Array, mode?: string | number): Promise<TreeItem>,
+   *  remove(file: string): Promise<TreeItem>,
+   *  commit(message: string): Promise<CommitData>
+   * }} Committer
+   */
+
+  /** @typedef {NonNullable<Parameters<import('@octokit/rest').Octokit['rest']['git']['createTree']>[0]>['tree'][0]} TreeItem */
+
+  /** @typedef {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['getCommit']>>['data']} CommitData */
+
+  /**
+   * @param {PrepareParams} params
+   * @returns {Promise<Committer>}
+   */
+  async function webcommit({
+    owner, repo, branch,
+    fetch = defaultFetch(),
+    auth,
+    octokit }) {
+
+    const headers = {
+      ...(auth && { Authorization: `token ${auth}` }),
+      Accept: "application/vnd.github.v3+json"
+    };
+
+    /** @type {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['getRef']>>['data']} */
+    const ref = await (octokit ?
+      octokit.rest.git.getRef({ owner, repo, ref: `heads/${branch}` }).then(res => res.data) :
+      fetch(`https://api.github.com/repos/${owner}/${repo}/git/refs/heads/${branch}`, { headers }).then(x => x.json()));
+
+    /** @type {CommitData} */
+    const headCommit = await (octokit ?
+      octokit.rest.git.getCommit({ owner, repo, commit_sha: ref.object.sha }).then(res => res.data) :
+      fetch(`https://api.github.com/repos/${owner}/${repo}/git/commits/${ref.object.sha}`, { headers }).then(x => x.json()));
+
+    /**
+     * @type {TreeItem[]}
+     */
+    const tree = [];
+
+    return {
+      head: headCommit,
+      put, remove,
+      commit
+    };
+
+    /**
+     * @param {string} file
+     * @param {string | ArrayBuffer | Uint8Array} content
+     * @param {string | number} mode
+     */
+    async function put(file, content, mode) {
+      const encodedBlob = toBase64(content);
+      /** @type {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['createBlob']>>['data']} */
+      const blob = await (octokit ?
+        octokit.rest.git.createBlob({ owner, repo, content: encodedBlob, encoding: 'base64' }).then(x => x.data) :
+        fetch(`https://api.github.com/repos/${owner}/${repo}/git/blobs`, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({ content: encodedBlob, encoding: 'base64' })
+        }).then(x => x.json()));
+
+      /** @type {typeof tree[0]} */
+      const treeItem = {
+        path: file,
+        mode: /** @type {*} */(deriveMode(file, mode)),
+        type: 'blob',
+        sha: blob.sha
+      };
+
+      tree.push(treeItem);
+      return treeItem;
+    }
+
+    /**
+     * @param {string} file
+     */
+    async function remove(file) {
+
+      /** @type {typeof tree[0]} */
+      const treeItem = {
+        path: file,
+        mode: '100644',
+        type: 'commit',
+        sha: null
+      };
+
+      tree.push(treeItem);
+
+      return treeItem;
+    }
+
+    /**
+     * @param {string} message
+     */
+    async function commit(message) {
+      /** @type {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['createTree']>>['data']} */
+      const treeObj = await (octokit ?
+        octokit.rest.git.createTree({ owner, repo, tree, base_tree: headCommit.tree.sha }).then(x => x.data) :
+        fetch(`https://api.github.com/repos/${owner}/${repo}/git/trees`, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({ tree, base_tree: headCommit.tree.sha })
+        }).then(x => x.json()));
+
+      /** @type {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['createCommit']>>['data']} */
+      const commitObj = await (octokit ?
+        octokit.rest.git.createCommit({ owner, repo, message, tree: treeObj.sha, parents: [headCommit.sha] }).then(x => x.data) :
+        fetch(`https://api.github.com/repos/${owner}/${repo}/git/commits`, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({ message, tree: treeObj.sha, parents: [headCommit.sha] })
+        }).then(x => x.json()));
+
+      /** @type {Awaited<ReturnType<import('@octokit/rest').Octokit['rest']['git']['updateRef']>>['data']} */
+      const updatedRef = await (octokit ?
+        octokit.rest.git.updateRef({ owner, repo, ref: `heads/${branch}`, sha: commitObj.sha }) :
+        fetch(`https://api.github.com/repos/${owner}/${repo}/git/refs/heads/${branch}`, {
+          method: 'PATCH',
+          headers,
+          body: JSON.stringify({ sha: commitObj.sha })
+        }).then(x => x.json()));
+
+      return commitObj;
+    }
+
+  }
+
+  /**
+   * @param {string} file
+   * @param {string | number} mode
+   */
+  function deriveMode(file, mode) {
+    if (!mode) return '100644';
+    else if (typeof mode === 'number') return mode.toString(8);
+    else return mode; // TODO: handle 'r', 'w', 'x' and 'x+' modes
+  }
+
+  /**
+   * @param {string | ArrayBuffer | Uint8Array} content
+   */
+  function toBase64(content) {
+    if (typeof content === 'string') return btoa(content);
+
+    const arr = content instanceof Uint8Array ? content : new Uint8Array(content);
+    let result = '';
+    for (let i = 0; i < arr.length; i++) {
+      result += String.fromCharCode(arr[i]);
+    }
+    return btoa(result);
+  }
+
+  function defaultFetch() {
+    // can put a polyfill here
+    return fetch;
   }
 
 
